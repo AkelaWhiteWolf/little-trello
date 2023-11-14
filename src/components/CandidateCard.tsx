1d3abccc76244e174e62e516bea806bcf1545429
Alexey Shvets <alexey199822la@gmail.com>
2023-11-14 13:41:46 +0200

    main/drag-n-drop created

--- src/components/CandidateCard.tsx
+++ src/components/CandidateCard.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { DragEvent } from 'react';
 import styles from 'src/styles/CandidateCard.module.scss';
 
 type Props = {
   firstName: string;
   lastName: string;
   order: string;
+  id: number;
 };
 
-export const CandidateCard: React.FC<Props> = ({
-  firstName,
-  lastName,
-  order,
-}) => {
+export const CandidateCard: React.FC<Props> = ({ firstName, lastName, id }) => {
   const fullName = `${firstName} ${lastName}`;
 
-  return <div className={styles['candidate-card']}>{fullName}</div>;
+  const onDragStart = (event: DragEvent<HTMLDivElement>) => {
+    event.dataTransfer.setData('id', String(id));
+  };
+  const onDragOver = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div
+      draggable
+      className={styles['candidate-card']}
+      onDragStart={onDragStart}
+      onDragOver={onDragOver}
+    >
+      {fullName}
+    </div>
+  );
 };
--- src/components/CandidateStatusColumn.tsx
+++ src/components/CandidateStatusColumn.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { DragEvent } from 'react';
+import { observer } from 'mobx-react-lite';
 import { Flex } from 'antd';
+import { candidatesStore } from 'src/store';
 import { getCandidatesByStatus } from 'src/utils';
 import { CandidateCard, ColumnStatusName } from 'src/components';
 import styles from 'src/styles/CandidateStatusColumn.module.scss';
@@ -9,23 +11,47 @@ type Props = {
   name: string;
 };
 
-export const CandidateStatusColumn: React.FC<Props> = ({ order, name }) => {
-  const candidatesOfThisColumn = getCandidatesByStatus(order);
+export const CandidateStatusColumn: React.FC<Props> = observer(
+  ({ order, name }) => {
+    const candidatesOfThisColumn = getCandidatesByStatus(order);
 
-  return (
-    <Flex vertical>
-      <ColumnStatusName name={name} />
+    const onDrop = (event: DragEvent<HTMLDivElement>) => {
+      event.preventDefault();
+      const id = Number(event.dataTransfer.getData('id'));
 
-      <Flex vertical gap="15px" className={styles['candidate-status-column']}>
-        {candidatesOfThisColumn.map(({ candidate, employerStatusId }) => (
-          <CandidateCard
-            key={candidate.id}
-            firstName={candidate.firstName}
-            lastName={candidate.lastName}
-            order={employerStatusId}
-          />
-        ))}
+      candidatesStore.changeEmployerStatus(id, order);
+      // FIXME: better to save previous border
+      event.currentTarget.style.border = '';
+    };
+    const onDragOver = (event: DragEvent<HTMLDivElement>) => {
+      event.preventDefault();
+      event.currentTarget.style.border = '1px solid lightblue';
+    };
+    const onDragLeave = (event: DragEvent<HTMLDivElement>) => {
+      event.currentTarget.style.border = '';
+    };
+
+    return (
+      <Flex vertical>
+        <ColumnStatusName name={name} />
+
+        <div
+          className={styles['candidate-status-column']}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          onDragLeave={onDragLeave}
+        >
+          {candidatesOfThisColumn.map(({ candidate, employerStatusId }) => (
+            <CandidateCard
+              key={candidate.id}
+              id={candidate.id}
+              firstName={candidate.firstName}
+              lastName={candidate.lastName}
+              order={employerStatusId}
+            />
+          ))}
+        </div>
       </Flex>
-    </Flex>
-  );
-};
+    );
+  },
+);
